dee19b135d54dc0885c722b0c047191b04a5a288
RoemIko <45184251+RoemIko@users.noreply.github.com>
2020-01-09 15:17:51 +0100

    update

---
+++ translate.js
@@ -0,0 +1,7 @@
+function GetSelectedValue()  {
+  var lanNumber = document.getElementById("country");
+  var option = lanNumber.options[lanNumber.selectedIndex].value;
+  var en = ["Captive portal", "Login to gain access to internet", "Name", "Ticket Number", "We'll never share your info with someone else", "I accept the terms and service", "Help", "Why can't I access internet?", "Where can I find my ticketnumber?"];
+  var nl = ["Inlog pagina", "Log in om toegang te krijgen tot internet", "Naam", "Ticket nummer", "Wij zullen nooit informatie delen met andere partijen", "Ik accepteer de algemene voorwaarden", "Help", "Waarom kan ik niet verbinden met internet?", "Waar kan ik mijn ticketnummer vinden?"];
+  var tr = [""];
+}
