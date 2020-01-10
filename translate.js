337638973c2a9248abbcb004818b585279b101f2
RoemIko <45184251+RoemIko@users.noreply.github.com>
2020-01-10 14:29:36 +0100

    DONE FINISHED WEG GONE LOES

--- translate.js
+++ translate.js
@@ -1,33 +1,55 @@
-window.onload = function() {
-  GetSelectedValue();
-  translate();
+var dataReload = document.querySelectorAll("[data-reload]");
 
-  function GetSelectedValue() {
-    var lanNumber = document.getElementById("country");
-    var option = lanNumber.options[lanNumber.selectedIndex].value;
+var language = {
+  eng: {
+    portal: "Captive Portal",
+    gain: "Login to gain access to internet",
+    naam: "Name:",
+    ticketval: "Ticket number:",
+    terms: "Corendon will never share your information with third parties.",
+    info: "I accept the terms and service"
+    /*welcome: */
+  },
+  nl: {
+    portal: "Inlog pagina",
+    gain: "Login om toegang te krijgen tot het internet",
+    naam: "Naam",
+    ticketval: "Ticket nummer:",
+    terms: "Corendon zal geen informatie delen met derden.",
+    info: "Ik accepteer de licentie overeenkomst"
+  },
+  tr: {
+    portal: "Kısıtlama portalı",
+    gain: "Internet’e bağlanmak için lütfen isminizi ve soyisminizi giriniz.",
+    naam: "Ad:",
+    ticketval: "Billet numarasi:",
+    terms: "Corendon'un üçüncü taraflarla bilgi paylaşmayacağınin.",
+    info: "lisans sözleşmesini kabul ediyorum."
   }
-
-
-
-    function translate() {
-      var en = ["Captive portal", "Login to gain access to internet", "Name", "Ticket Number", "We'll never share your info with someone else", "I accept the terms and service", "Help", "Why can't I access internet?", "Where can I find my ticketnumber?"];
-      var nl = ["Inlog pagina", "Log in om toegang te krijgen tot internet", "Naam", "Ticket nummer", "Wij zullen nooit informatie delen met andere partijen", "Ik accepteer de algemene voorwaarden", "Help", "Waarom kan ik niet verbinden met internet?", "Waar kan ik mijn ticketnummer vinden?"];
-      var tr = [""];
-
-      if (option = "NL") {
-        document.getElementById('Captive tekst').innerHTML = 'hi';
-        /*document.getElementById("Login melding").innerHTML = nl[1];
-        document.getElementById("Naam").innerHTML = nl[2];
-        document.getElementById("Ticketnummer").innerHTML = nl[3];
-        document.getElementById("ticketHelp").innerHTML = nl[4];
-        document.getElementById("Acceptatie").innerHTML = nl[5];
-        document.getElementById("Help").innerHTML = nl[6];
-        document.getElementById("No internet").innerHTML = nl[7];
-        document.getElementById("Zoek ticketnummer").innerHTML = nl[8];
-        document.getElementById("sluiten").innerHTML = nl[9];*/
-      }
-      if (option = "EN") {
-        document.getElementById('Captive tekst').innerHTML = 'thot';
-      }
-    }
+};
+if (window.location.hash) {
+  if (window.location.hash === "#nl") {
+    captive.textContent = language.nl.portal;
+    login.textContent = language.nl.gain;
+    waardenaam.textContent = language.nl.naam;
+    waarde.textContent = language.nl.ticketval;
+    ticketHelp.textContent = language.nl.terms;
+    acceptatie.textContent = language.nl.info;
   }
+}
+if (window.location.hash) {
+  if (window.location.hash === "#tr") {
+    captive.textContent = language.tr.portal;
+    login.textContent = language.tr.gain;
+    waardenaam.textContent = language.tr.naam;
+    waarde.textContent = language.tr.ticketval;
+    ticketHelp.textContent = language.tr.terms;
+    acceptatie.textContent = language.tr.info;
+  }
+}
+for (let i = 0; i < dataReload.length; i++) {
+  dataReload[i].onclick = function() {
+    window.location.hash = dataReload[i].hash;
+    window.location.reload(true);
+  }
+}
